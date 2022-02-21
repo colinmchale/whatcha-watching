f332cb3484b655c797f1bba58aeda1d77656d3d5
Colin McHale <93059063+colinmchale@users.noreply.github.com>
2022-02-21 17:29:11 -0600

    set up basic api fetch for search and started to work in handling changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
+import React, { useState } from "react";
 import "bulma/css/bulma.min.css";
 import RenderResult from "./components/RenderResult";
 import SearchArea from "./components/SerachArea";
 
 function App() {
 
+  const [country, setCountry] = useState('us');
+  const [language, setLanguage] = useState('en');
+  const [lightMode, setLightMode] = useState('light');
 
   return (
     <div>
-      <div className="columns is-centered is-vcentered"> 
-      <h1 className="title is-1 is">Whatcha Watching?</h1>
+      <div>
+      <nav className="navbar is-transparent">
+        <div className="navbar-end navbar-item has-dropdown is-hoverable">
+          <span className="navbar-link">
+            Settings
+          </span>
+          <div className="navbar-dropdown is-right is-boxed">
+            <a className="navbar-item" href="https://bulma.io/documentation/overview/start/">
+              Language
+            </a>
+            <a className="navbar-item" href="https://bulma.io/documentation/overview/modifiers/">
+              Country
+            </a>
+            <a className="navbar-item" href="https://bulma.io/documentation/columns/basics/">
+              Dark Mode
+            </a>
+          </div>
+        </div>
+      </nav>
       </div>
-      <SearchArea/>
-      <RenderResult/>
+      <SearchArea
+      country={country}
+      language={language}
+      />
     </div>
   );
 }
--- src/components/SerachArea.js
+++ src/components/SerachArea.js
@@ -1,60 +1,137 @@
 import React, { useState } from "react";
-
+import RenderResult from "./RenderResult";
+import axios from "axios";
 
 const SearchArea = () => {
 
-    return (
-    <div className="columns is-mobile is-centered">
+  const [type, setType] = useState('Movie');
+  const [genre, setGenre] = useState('');
+  const [keyword, setKeyword] = useState('');
+  const [streamService, setStreamService] = useState('');
+
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(type); 
+    console.log(genre);
+    console.log(streamService);
+    console.log(keyword);
+
+    // let options = {
+    //   method: 'GET',
+    //   url: 'https://streaming-availability.p.rapidapi.com/search/basic',
+    //   params: {
+    //     country: 'us',
+    //     service: 'netflix',
+    //     type: 'movie',
+    //     genre: '18',
+    //     page: '1',
+    //     output_language: 'en',
+    //     language: 'en'
+    //   },
+    //   headers: {
+    //     'x-rapidapi-host': 'streaming-availability.p.rapidapi.com',
+    //     'x-rapidapi-key': 'b80c64fe9emshef27d98ce1923b6p1f4017jsn1bb4451e68ed'
+    //   }
+    // };
+
+    // axios.request(options).then(function (response) {
+    //   console.log(response.data);
+    // }).catch(function (error) {
+    //   console.error(error);
+    // });
+  };
+
+  // function getMoives(type, genre, streamService, keyword) {
+
+  // }
+
+  const handleTypeChange = (event) => {
+    const { name, value } = event.target;
+    // console.log(name);
+    // console.log(value);
+    setType(value);
+  };
+
+  const handleGenreChange = (event) => {
+    const { name, value } = event.target;
+    // console.log(name);
+    // console.log(value);
+    setGenre(value);
+  };
+
+  const handleKeywordChange = (event) => {
+    const { name, value } = event.target;
+    // console.log(name);
+    // console.log(value);
+    setKeyword(value);
+  };
+
+  return (
+    <div>
+      <div className="columns is-centered is-vcentered"> 
+      <h1 className="title is-1 is">Whatcha Watching?</h1>
+      </div>
+      <div className="columns is-mobile is-centered">
         <div className="column is-6 is-offset-3">
-        <div className="field">
-          <label className="label">Select Type:</label>
-          <div className="control">
-            <div className="select is-primary">
-              <select>
-                <option>Movie</option>
-                <option>Series</option>
-              </select>
+          <div className="field">
+            <label className="label">Select Type:</label>
+            <div className="control">
+              <div className="select is-primary">
+                <select onChange={handleTypeChange}>
+                  <option>Movie</option>
+                  <option>Series</option>
+                </select>
+              </div>
+            </div>
+          </div>
+
+          <div className="field">
+            <label className="label">Select Genre:</label>
+            <div className="control">
+              <div className="select is-primary">
+                <select onChange={handleGenreChange}>
+                  <option>Action</option>
+                  <option>Adventure</option>
+                  <option>Comedy</option>
+                  <option>Horror</option>
+                  <option>Mystery</option>
+                  <option>Romance</option>
+                  <option>Sci-Fi</option>
+                  <option>Thriller</option>
+                </select>
+              </div>
+            </div>
+          </div>
+
+          <div className="field">
+            <label className="label">Keyword:</label>
+            <div className="control">
+              <input
+                className="input is-primary"
+                onChange={handleKeywordChange}
+                type="text"
+                placeholder="Type Here"
+              />
+            </div>
+          </div>
+
+          <div className="field is-grouped">
+            <div className="control">
+              <button className="button is-danger" onClick={handleFormSubmit}>
+                Submit
+              </button>
             </div>
           </div>
         </div>
+      </div>
 
-        <div className="field">
-          <label className="label">Select Genre:</label>
-          <div className="control">
-            <div className="select is-primary">
-              <select>
-                <option>Action</option>
-                <option>Adventure</option>
-                <option>Comedy</option>
-                <option>Horror</option>
-                <option>Mystery</option>
-                <option>Romance</option>
-                <option>Sci-Fi</option>
-                <option>Thriller</option>
-              </select>
-            </div>
-          </div>
-        </div>
-
-        <div className="field">
-          <label className="label">Keyword:</label>
-          <div className="control">
-            <input
-              className="input is-primary"
-              type="text"
-              placeholder="Type Here"
-            />
-          </div>
-        </div>
-
-        <div class="field is-grouped">
-            <div class="control">
-            <button class="button is-danger">Submit</button>
-            </div>
-        </div>
-        </div>
+      <div>
+        <RenderResult />
+      </div>
     </div>
-    )
-}
+  );
+};
 
 export default SearchArea;
