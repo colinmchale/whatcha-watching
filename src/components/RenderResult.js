818a2b5a4efd06dcecf507eff6995bbfd4f02688
Colin McHale <93059063+colinmchale@users.noreply.github.com>
2022-02-21 20:30:10 -0600

    added genreLibrary and trying to fix search api with correct params

--- src/components/RenderResult.js
+++ src/components/RenderResult.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 
-const RenderResult = () => {
+const RenderResult = ({}) => {
 
     return (
         <div className="columns is-mobile">
--- src/components/SerachArea.js
+++ src/components/SerachArea.js
@@ -4,11 +4,41 @@ import axios from "axios";
 
 const SearchArea = () => {
 
-  const [type, setType] = useState('Movie');
+  const [type, setType] = useState('');
   const [genre, setGenre] = useState('');
-  const [keyword, setKeyword] = useState('');
   const [streamService, setStreamService] = useState('');
+  const [keyword, setKeyword] = useState('');
 
+  const genreLibrary = {
+    "Biography": "1",
+    "Film Noir": "2",
+    "Game Show": "3",
+    "Musical": "4",
+    "Sport": "5",
+    "Short": "6",
+    "Adult": "7",
+    "Adventure": "12",
+    "Fantasy": "14",
+    "Animation": "16",
+    "Drama": "18",
+    "Horror": "27",
+    "Action": "28",
+    "Comedy": "35",
+    "History": "36",
+    "Western": "37",
+    "Thriller": "53",
+    "Crime": "80",
+    "Documentary": "99",
+    "Science Fiction": "878",
+    "Mystery": "9648",
+    "Music": "10402",
+    "Romance": "10749",
+    "Family": "10751",
+    "War": "10752",
+    "News": "10763",
+    "Reality": "10764",
+    "Talk Show": "10767"
+  }
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
@@ -18,54 +48,65 @@ const SearchArea = () => {
     console.log(streamService);
     console.log(keyword);
 
-    // let options = {
-    //   method: 'GET',
-    //   url: 'https://streaming-availability.p.rapidapi.com/search/basic',
-    //   params: {
-    //     country: 'us',
-    //     service: 'netflix',
-    //     type: 'movie',
-    //     genre: '18',
-    //     page: '1',
-    //     output_language: 'en',
-    //     language: 'en'
-    //   },
-    //   headers: {
-    //     'x-rapidapi-host': 'streaming-availability.p.rapidapi.com',
-    //     'x-rapidapi-key': 'b80c64fe9emshef27d98ce1923b6p1f4017jsn1bb4451e68ed'
-    //   }
-    // };
+    let options = {
+      method: 'GET',
+      url: 'https://streaming-availability.p.rapidapi.com/search/basic',
+      params: {
+        country: 'us',
+        service: streamService,
+        type: type,
+        genre: '18',
+        page: '1',
+        output_language: 'en',
+        language: 'en'
+      },
+      headers: {
+        'x-rapidapi-host': 'streaming-availability.p.rapidapi.com',
+        'x-rapidapi-key': 'b80c64fe9emshef27d98ce1923b6p1f4017jsn1bb4451e68ed'
+      }
+    };
 
-    // axios.request(options).then(function (response) {
-    //   console.log(response.data);
-    // }).catch(function (error) {
-    //   console.error(error);
-    // });
+    axios.request(options).then(function (response) {
+      console.log(response.data);
+    }).catch(function (error) {
+      console.error(error);
+    });
   };
-
-  // function getMoives(type, genre, streamService, keyword) {
-
-  // }
 
   const handleTypeChange = (event) => {
     const { name, value } = event.target;
     // console.log(name);
     // console.log(value);
-    setType(value);
+    let lowerType = value.toLowerCase();
+    console.log(lowerType);
+    setType(lowerType);
   };
 
   const handleGenreChange = (event) => {
     const { name, value } = event.target;
     // console.log(name);
     // console.log(value);
-    setGenre(value);
+    let genre = genreLibrary[value];
+    console.log(genre);
+    setGenre(genre);
+  };
+
+  const handleStreamServiceChange = (event) => {
+    const { name, value } = event.target;
+    // console.log(name);
+    // console.log(value);
+    let lowerStream = value.toLowerCase();
+    console.log(lowerStream);
+    setStreamService(lowerStream);
   };
 
   const handleKeywordChange = (event) => {
     const { name, value } = event.target;
     // console.log(name);
     // console.log(value);
-    setKeyword(value);
+    let lowerKeyword = value.toLowerCase();
+    console.log(lowerKeyword);
+    setKeyword(lowerKeyword);
   };
 
   return (
@@ -106,7 +147,22 @@ const SearchArea = () => {
           </div>
 
           <div className="field">
-            <label className="label">Keyword:</label>
+            <label className="label">Select Streaming Service:</label>
+            <div className="control">
+              <div className="select is-primary">
+                <select onChange={handleStreamServiceChange}>
+                  <option>Netflix</option>
+                  <option>Hulu</option>
+                  <option>Prime</option>
+                  <option>HBO</option>
+                  <option>ShowTime</option>
+                </select>
+              </div>
+            </div>
+          </div>
+
+          <div className="field">
+            <label className="label">Keyword (optional):</label>
             <div className="control">
               <input
                 className="input is-primary"
